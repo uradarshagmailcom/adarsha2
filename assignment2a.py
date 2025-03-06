7495d0aed304f3aa96a97b2bf780d1822d35872a
uradarshagmailcom <58351518+uradarshagmailcom@users.noreply.github.com>
2025-03-06 13:38:50 +0530

    Add files via upload

---
+++ assignment2a.py
@@ -0,0 +1,6 @@
+# number even or odd
+num=int(input('Enter a number: '))
+if num%2==0:
+    print(num, " is an even number")
+else:
+    print(num, 'is a odd number')
---
+++ assignment2b.py
@@ -0,0 +1,5 @@
+#sum of 1 to 50 
+sum = 0
+for i in range(1,51):
+    sum = sum + i
+print('The sum of the number 1 to 50 is: ',sum)
